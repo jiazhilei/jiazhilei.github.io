fc01d9ae151a1afc68200303aec434cca8f21ef0
jiazhilei <81570055+jiazhilei@users.noreply.github.com>
2024-05-15 13:37:15 +0000

    feat: 取消摘要

--- .vuepress/config.ts
+++ .vuepress/config.ts
@@ -8,7 +8,7 @@ export default defineUserConfig({
   lang: "zh-CN",
   title: "博客演示",
   description: "vuepress-theme-hope 的博客演示",
-
+  
   theme,
   plugins: [
     shikiPlugin({
--- .vuepress/theme.ts
+++ .vuepress/theme.ts
@@ -95,7 +95,10 @@ export default hopeTheme({
 
   // 在这里配置主题提供的插件
   plugins: {
-    blog: true,
+    blog: {
+      // 摘要
+      excerpt: false,
+    },
     prismjs: false,
     copyCode: true, // 复制
     // photoSwipe: false, // 图片预览
